139ca41c438bf5d09829b7b75b70ad44c28ae9d7
Daniel Faustino <danieloliveirafaustino@gmail.com>
2021-06-23 22:46:36 -0300

    middleware in class

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import 'reflect-metadata';
-import express, { Request, Response, NextFunction } from 'express';
+import express from 'express';
 import 'express-async-errors';
 import { router } from './routes';
 import './database';
+import { middlewares } from './middleware';
 
+const middleware = new middlewares();
 
 const app = express();
 
@@ -11,22 +13,6 @@ app.use(express.json());
 
 app.use(router);
 
-app.use(
-  (err: Error, request: Request, response: Response, next: NextFunction) => {
-    if (err instanceof Error) {
-      return response.status(400).json({
-        error: err.message,
-      });
-    }
-
-    return response.status(500).json({
-      status: 'error',
-      message: 'Internal Server Error',
-    });
-
-    next()
-  }
-  
-);
+app.use(middleware.errors);
 
 app.listen(3000, () => console.log('Server is Running'));
